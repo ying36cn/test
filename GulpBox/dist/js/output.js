cabb69c13ba62122b9cb097179513486c1ce951c
ying36cn <ying36cn@sina.com>
2019-12-09 23:31:40 +0800

    Gulp基础

---
+++ GulpBox/dist/js/output.js
@@ -0,0 +1,1 @@
+"use strict";document.querySelector("h1").style.background="yellow";var h2=document.createElement("h2");h2.innerHTML="gulp course!",document.body.appendChild(h2);
---
+++ GulpBox/src/script/sub.js
@@ -0,0 +1,3 @@
+let h2Box = document.createElement('h2');
+h2Box.innerHTML = 'gulp course!';
+document.body.appendChild(h2Box);
